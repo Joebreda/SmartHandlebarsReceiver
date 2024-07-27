44eb0098426d90cdc479c1a3fff0822c70fc2e05
Joe Breda <joebreda@MacBook-Pro-272.local>
2024-07-26 17:27:38 -0700

    before location service update

--- app/src/main/java/com/ubicomplab/biketofreceiver/MainActivity.java
+++ app/src/main/java/com/ubicomplab/biketofreceiver/MainActivity.java
@@ -787,6 +787,7 @@ public class MainActivity extends AppCompatActivity {
         String[] permissions = new String[]{
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
                 Manifest.permission.BLUETOOTH,
                 Manifest.permission.BLUETOOTH_ADMIN,
                 Manifest.permission.BLUETOOTH_CONNECT,
